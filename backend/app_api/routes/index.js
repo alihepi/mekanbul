f5312dc3dc952f1de6ea7a7a1e8e94538bb7970b
Ali Mutlu <95170323+alihepi@users.noreply.github.com>
2024-01-06 21:33:54 +0300

    final-version

--- backend/app_api/routes/index.js
+++ backend/app_api/routes/index.js
@@ -1,51 +1,52 @@
-var express = require("express");
-var router = express.Router();
-var ctrlVenues = require("../controllers/VenueController");
-var ctrlComments = require("../controllers/CommentController");
+var express=require("express");
+var router=express.Router();
+var ctrlVenues=require("../controllers/VenueController");
+var ctrlComments=require("../controllers/CommentController");
 
 const ctrlAuth = require('../controllers/Auth');
 const jwt = require('express-jwt');
 
-const auth = jwt({
-    secret: process.env.JWT_SECRET,
+const auth = jwt.expressjwt({
+    secret:process.env.JWT_SECRET,
     userProperty: "payload",
     algorithms: ["sha1", "RS256", "HS256"]
 });
 
 router
-    .route("/venues")
-    .get(ctrlVenues.listNearbyVenues)
-    .post(ctrlVenues.addVenue);
+.route("/venues")
+.get(ctrlVenues.listNearbyVenues)
+.post(ctrlVenues.addVenue);
 
 router
-    .route("/venues/:venueid")
-    .get(ctrlVenues.getVenue)
-    .put(ctrlVenues.updateVenue)
-    .delete(ctrlVenues.deleteVenue);
+.route("/venues/:venueid")
+.get(ctrlVenues.getVenue)
+.put(ctrlVenues.updateVenue)
+.delete(ctrlVenues.deleteVenue);
 
 router
-    .route("/venues/:venueid/comments")
-    .post(auth, ctrlComments.addComment);
+.route("/venues/:venueid/comments")
+.post(auth, ctrlComments.addComment);
 
 router
-    .route("/venues/:venueid/comments/:commentid")
-    .get(ctrlComments.getComment)
-    .put(auth, ctrlComments.updateComment)
-    .delete(auth, ctrlComments.deleteComment);
+.route("/venues/:venueid/comments/:commentid")
+.get(ctrlComments.getComment)
+.put(auth, ctrlComments.updateComment)
+.delete(auth, ctrlComments.deleteComment);
+
 
 /*Admin*/
 router
-    .route("/admin")
-    .get(ctrlVenues.listAllVenues);
+.route("/admin")
+.get(ctrlVenues.listAllVenues);
 
 router
-    .route("/admin/addupdate/venue/:venueid")
-    .get(ctrlVenues.getVenue)
-    .put(ctrlVenues.updateVenue);
+.route("/admin/addupdate/venue/:venueid")
+.get(ctrlVenues.getVenue)
+.put(ctrlVenues.updateVenue);
 
 router
-    .route("/admin/addupdate/venue/new")
-    .post(ctrlVenues.addVenue);
+.route("/admin/addupdate/venue/new")
+.post(ctrlVenues.addVenue);
 
 /*JWT*/
 router.post("/login", ctrlAuth.login);
@@ -53,4 +54,4 @@ router.post("/login", ctrlAuth.login);
 /*Register*/
 router.post("/signup", ctrlAuth.signUp);
 
-module.exports = router;
+module.exports=router;
